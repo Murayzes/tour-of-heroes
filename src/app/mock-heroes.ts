966a5d73a4655441259ed78efa55c09a9b0130ad
Murayzes <murayzes@gmail.com>
2019-01-13 14:05:09 +0200

    i_added_base_of_names

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Vladislav'},
+    { id: 2, name: 'Marcos'},
+    { id: 3, name: 'Illya'},
+    { id: 4, name: 'Andy'},
+    { id: 5, name: 'Artur'},
+    { id: 6, name: 'Natasha'},
+    { id: 7, name: 'Dima'},
+    { id: 8, name: 'Grigiriy'},
+    { id: 9, name: 'Lenya'},
+    { id: 10, name: 'Volodimir'},
+    { id: 11, name: 'Stanislav'},
+    { id: 12, name: 'Irina'},
+    { id: 13, name: 'Yuriy'}
+]
