97cb30faf8b09e514b0df2c07a091956e8e6e894
Murayzes <murayzes@gmail.com>
2019-02-06 15:00:49 +0200

    i_create_app-messages_and_service_for_him

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // NgModel
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
+  AppComponent,
+  HeroesComponent,
+  HeroDetailComponent,
+  MessagesComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+  BrowserModule,
+  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,32 @@
 import { OnInit, Component } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+import { HeroService } from "../hero.service";
 
 @Component({
-    selector: 'app-heroes',
-    templateUrl: './heroes.component.html',
-    styleUrls: ['./heroes.component.css']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
 
-    selectedHero: Hero;
-    heroes = HEROES;
+	selectedHero: Hero;
 
-    constructor() { }   
+	heroes: Hero[];
 
-    ngOnInit() {
-    }
+	constructor(private heroService: HeroService) { }   
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    } 
+	ngOnInit() {
+		this.getHeroes();
+	}
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+
+	getHeroes(): void {
+		this.heroService.getHeroes()
+		.subscribe(heroes => this.heroes = heroes);
+	};
 };
