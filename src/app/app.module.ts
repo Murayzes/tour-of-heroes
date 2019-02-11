c4c97760070bfcf183fbf317f0081a239b7916d8
Murayzes <murayzes@gmail.com>
2019-02-11 15:27:15 +0200

    i_add_routing_module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
   AppComponent,
   HeroesComponent,
   HeroDetailComponent,
-  MessagesComponent
+  MessagesComponent,
+  DashboardComponent
   ],
   imports: [
   BrowserModule,
-  FormsModule
+  FormsModule,
+  AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,17 +4,22 @@ import { Observable, of } from "rxjs";
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { MessageService } from "./message.service";
+import { MessageService } from "./message.service"
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of (HEROES.find(hero => hero.id === id));
+  }
 }
